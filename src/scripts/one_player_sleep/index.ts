d59646b3c3705fa280ddd7d6b1de8a6315f40f6f
dudushy <eduardotalarico1337@gmail.com>
2024-06-22 16:29:03 -0300

    Add new consts COLOR and TITLE

--- src/scripts/one_player_sleep/index.ts
+++ src/scripts/one_player_sleep/index.ts
@@ -3,6 +3,9 @@ import {
   system
 } from "@minecraft/server";
 
+const COLOR = "§9";
+const TITLE = "One Player Sleep§r";
+
 const START_TICK = 100;
 
 let currentTick = 0;
@@ -12,11 +15,11 @@ function gameTick() {
     currentTick++;
 
     if (currentTick === START_TICK) {
-      world.sendMessage("§9One Player Sleep§r Script Loaded!");
+      world.sendMessage(`${COLOR}${TITLE} Script Loaded!`);
 
       world.gameRules.playersSleepingPercentage = 0;
 
-      world.sendMessage("[@] §9playersSleepingPercentage§r: " + world.gameRules.playersSleepingPercentage);
+      world.sendMessage(`[@] ${COLOR}playersSleepingPercentage§r: ` + world.gameRules.playersSleepingPercentage);
     }
   } catch (e) {
     console.warn("Tick error: " + e);
