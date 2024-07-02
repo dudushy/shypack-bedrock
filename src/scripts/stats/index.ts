f97a47621e133a859c8fe63ab9e53748eccfe62b
dudushy <eduardotalarico1337@gmail.com>
2024-07-01 23:20:39 -0300

    Update index.ts

--- src/scripts/stats/index.ts
+++ src/scripts/stats/index.ts
@@ -1,12 +1,4 @@
-import {
-  world,
-  system,
-  DisplaySlotId,
-  ScoreboardObjective,
-  PlayerBreakBlockAfterEvent,
-  PlayerPlaceBlockAfterEvent,
-  Player
-} from "@minecraft/server";
+import * as server from "@minecraft/server";
 
 const DEBUG = true;
 
@@ -25,109 +17,110 @@ const MINUTE = 20 * 60;
 const HOUR = MINUTE * 60;
 const SIDEBAR_TIMEOUT = SECOND * 30;
 
-var debug: ScoreboardObjective;
-var ticks: ScoreboardObjective;
-var hours_played: ScoreboardObjective;
-var deaths: ScoreboardObjective;
-var blocks_broken: ScoreboardObjective;
-var blocks_placed: ScoreboardObjective;
-var items_used: ScoreboardObjective;
-var kills: ScoreboardObjective;
-var current_level: ScoreboardObjective;
-var max_level: ScoreboardObjective;
-var pearls_used: ScoreboardObjective;
+const debug = server.world.scoreboard.getObjective("debug") || server.world.scoreboard.addObjective("debug", "Debug");
+const ticks = server.world.scoreboard.getObjective("ticks") || server.world.scoreboard.addObjective("ticks", "Ticks");
+const hours_played = server.world.scoreboard.getObjective("hours_played") || server.world.scoreboard.addObjective("hours_played", "Hours Played");
+const deaths = server.world.scoreboard.getObjective("deaths") || server.world.scoreboard.addObjective("deaths", "Deaths");
+const blocks_broken = server.world.scoreboard.getObjective("blocks_broken") || server.world.scoreboard.addObjective("blocks_broken", "Blocks Broken");
+const blocks_placed = server.world.scoreboard.getObjective("blocks_placed") || server.world.scoreboard.addObjective("blocks_placed", "Blocks Placed");
+const items_used = server.world.scoreboard.getObjective("items_used") || server.world.scoreboard.addObjective("items_used", "Items Used");
+const kills = server.world.scoreboard.getObjective("kills") || server.world.scoreboard.addObjective("kills", "Kills");
+const current_level = server.world.scoreboard.getObjective("current_level") || server.world.scoreboard.addObjective("current_level", "Current Level");
+const max_level = server.world.scoreboard.getObjective("max_level") || server.world.scoreboard.addObjective("max_level", "Max Level");
+const pearls_used = server.world.scoreboard.getObjective("pearls_used") || server.world.scoreboard.addObjective("pearls_used", "Pearls Used");
 
-var sidebarObjectiveArray: any[] = [];
+const sidebarObjectiveArray = [
+  debug,
+  ticks,
+  hours_played,
+  deaths,
+  blocks_broken,
+  blocks_placed,
+  items_used,
+  kills,
+  current_level,
+  max_level,
+  pearls_used
+];
 
-function initializeScoreboards() {
-  debug = world.scoreboard.getObjective("debug") || world.scoreboard.addObjective("debug", "Debug");
-  ticks = world.scoreboard.getObjective("ticks") || world.scoreboard.addObjective("ticks", "Ticks");
-  hours_played = world.scoreboard.getObjective("hours_played") || world.scoreboard.addObjective("hours_played", "Hours Played");
-  deaths = world.scoreboard.getObjective("deaths") || world.scoreboard.addObjective("deaths", "Deaths");
-  blocks_broken = world.scoreboard.getObjective("blocks_broken") || world.scoreboard.addObjective("blocks_broken", "Blocks Broken");
-  blocks_placed = world.scoreboard.getObjective("blocks_placed") || world.scoreboard.addObjective("blocks_placed", "Blocks Placed");
-  items_used = world.scoreboard.getObjective("items_used") || world.scoreboard.addObjective("items_used", "Items Used");
-  kills = world.scoreboard.getObjective("kills") || world.scoreboard.addObjective("kills", "Kills");
-  current_level = world.scoreboard.getObjective("current_level") || world.scoreboard.addObjective("current_level", "Current Level");
-  max_level = world.scoreboard.getObjective("max_level") || world.scoreboard.addObjective("max_level", "Max Level");
-  pearls_used = world.scoreboard.getObjective("pearls_used") || world.scoreboard.addObjective("pearls_used", "Pearls Used");
+server.system.run(gameTick);
 
-  sidebarObjectiveArray = [
-    debug,
-    ticks,
-    hours_played,
-    deaths,
-    blocks_broken,
-    blocks_placed,
-    items_used,
-    kills,
-    current_level,
-    max_level,
-    pearls_used
-  ];
-}
-
-// Ensure initializeScoreboards is called at the start
-initializeScoreboards();
-
-system.run(gameTick);
-
-world.afterEvents.entityDie.subscribe((event) => {
+server.world.afterEvents.entityDie.subscribe((event) => {
   try {
     if (event.deadEntity.typeId != 'minecraft:player') {
-      const player = event.damageSource.damagingEntity as Player;
-      if (!player) throw new Error("Damaging entity is not a player");
+      const player = event.damageSource.damagingEntity as server.Player;
+      if (!player) throw new Error("Player is undefined");
+
+      const playerIdentity = player.scoreboardIdentity;
+      // if (!playerIdentity) throw new Error("Player identity is undefined");
+      log(`${COLOR}[entityDie] Player identity is undefined`);
 
       const score = kills.getScore(player) || 0;
       kills.setScore(player, score + 1);
 
-      if (DEBUG) world.sendMessage(`[entityDie] ${player.name} ${COLOR}killed §r${event.deadEntity.typeId}${COLOR}!`);
+      log(`${COLOR}[entityDie] ${player.name} ${COLOR}killed §r${event.deadEntity.typeId}${COLOR}!`);
     } else {
-      const player = event.deadEntity as Player;
-      if (!player) throw new Error("Dead entity is not a player");
+      const player = event.deadEntity as server.Player;
+      if (!player) throw new Error("Player is undefined");
+
+      const playerIdentity = player.scoreboardIdentity;
+      // if (!playerIdentity) throw new Error("Player identity is undefined");
+      log(`${COLOR}[entityDie] Player identity is undefined`);
 
       const score = deaths.getScore(player) || 0;
       deaths.setScore(player, score + 1);
 
-      if (DEBUG) world.sendMessage(`[entityDie] ${player.name} ${COLOR}died!`);
+      log(`${COLOR}[entityDie] ${player.name} ${COLOR}died!`);
     }
   } catch (error) {
-    world.sendMessage(`${COLOR}[entityDie] ${COLOR_ERROR}Error: §r` + error);
+    log(`${COLOR}[entityDie] ${COLOR_ERROR}Error: §r` + error);
   }
 });
 
-world.afterEvents.playerBreakBlock.subscribe((event: PlayerBreakBlockAfterEvent) => {
+server.world.afterEvents.playerBreakBlock.subscribe((event: server.PlayerBreakBlockAfterEvent) => {
   try {
-    const player = event.player;
-    if (!player) throw new Error("Player not found in playerBreakBlock event");
+    const player = event.player as server.Player;
+    if (!player) throw new Error("Player is undefined");
+
+    const playerIdentity = player.scoreboardIdentity;
+    // if (!playerIdentity) throw new Error("Player identity is undefined");
+    log(`${COLOR}[playerBreakBlock] Player identity is undefined` + playerIdentity);
 
     const score = blocks_broken.getScore(player) || 0;
     blocks_broken.setScore(player, score + 1);
 
-    if (DEBUG) world.sendMessage(`[playerBreakBlock] ${player.name} ${COLOR}broke §r${event.brokenBlockPermutation.type.id}`);
+    log(`${COLOR}[playerBreakBlock] ${event.player.name} ${COLOR}broke §r${event.brokenBlockPermutation.type.id}`);
   } catch (error) {
-    world.sendMessage(`${COLOR}[playerBreakBlock] ${COLOR_ERROR}Error: §r` + error);
+    log(`${COLOR}[playerBreakBlock] ${COLOR_ERROR}Error: §r` + error);
   }
 });
 
-world.afterEvents.playerPlaceBlock.subscribe((event: PlayerPlaceBlockAfterEvent) => {
+server.world.afterEvents.playerPlaceBlock.subscribe((event: server.PlayerPlaceBlockAfterEvent) => {
   try {
-    const player = event.player;
-    if (!player) throw new Error("Player not found in playerPlaceBlock event");
+    const player = event.player as server.Player;
+    if (!player) throw new Error("Player is undefined");
+
+    const playerIdentity = player.scoreboardIdentity;
+    // if (!playerIdentity) throw new Error("Player identity is undefined");
+    log(`${COLOR}[playerPlaceBlock] Player identity is undefined`);
 
     const score = blocks_placed.getScore(player) || 0;
     blocks_placed.setScore(player, score + 1);
 
-    if (DEBUG) world.sendMessage(`[playerPlaceBlock] ${player.name} ${COLOR}placed §r${event.block.type.id}`);
+    log(`${COLOR}[playerPlaceBlock] ${player.name} ${COLOR}placed §r${event.block.type.id}`);
   } catch (error) {
-    world.sendMessage(`${COLOR}[playerPlaceBlock] ${COLOR_ERROR}Error: §r` + error);
+    log(`${COLOR}[playerPlaceBlock] ${COLOR_ERROR}Error: §r` + error);
   }
 });
 
-world.afterEvents.itemUse.subscribe((event) => {
+server.world.afterEvents.itemUse.subscribe((event) => {
   try {
-    const player = event.source as Player;
-    if (!player) throw new Error("Player not found in itemUse event");
+    const player = event.source as server.Player;
+    if (!player) throw new Error("Player is undefined");
+
+    const playerIdentity = player.scoreboardIdentity;
+    // if (!playerIdentity) throw new Error("Player identity is undefined");
+    log(`${COLOR}[itemUse] Player identity is undefined`);
 
     const score = items_used.getScore(player) || 0;
     items_used.setScore(player, score + 1);
@@ -137,23 +130,28 @@ world.afterEvents.itemUse.subscribe((event) => {
       pearls_used.setScore(player, score + 1);
     }
 
-    if (DEBUG) world.sendMessage(`[itemUse] ${player.name} ${COLOR}used §r${event.itemStack.type.id}`);
+    log(`${COLOR}[itemUse] ${player.name} ${COLOR}used §r${event.itemStack.type.id}`);
 
     if (DEBUG) cycleSidebar();
   } catch (error) {
-    world.sendMessage(`${COLOR}[itemUse] ${COLOR_ERROR}Error: §r` + error);
+    log(`${COLOR}[itemUse] ${COLOR_ERROR}Error: §r` + error);
   }
 });
+
+function log(message: string, force = false) {
+  if (DEBUG || force) server.world.sendMessage(message);
+  console.log(message);
+}
 
 function gameTick() {
   try {
     currentTick++;
 
     if (currentTick === START_TICK) {
-      world.sendMessage(`${COLOR}${TITLE} Script Loaded!` + (DEBUG ? ` §r§l§b${DEBUG_NUMBER}` : ""));
+      log(`${COLOR}${TITLE} Script Loaded!` + (DEBUG ? ` §r§l§b${DEBUG_NUMBER}` : ""), true);
 
-      if (DEBUG) world.sendMessage(`${COLOR}[gameTick] SIDEBAR_TIMEOUT: §r${SIDEBAR_TIMEOUT}`);
-      if (DEBUG) world.sendMessage(`${COLOR}[gameTick] sidebarObjectiveArray: §r(${sidebarObjectiveArray.length}) ${sidebarObjectiveArray.map(obj => obj.displayName).join(', ')}`);
+      log(`${COLOR}[gameTick] SIDEBAR_TIMEOUT: §r${SIDEBAR_TIMEOUT}`);
+      log(`${COLOR}[gameTick] sidebarObjectiveArray: §r(${sidebarObjectiveArray.length}) ${sidebarObjectiveArray.map(obj => obj.displayName).join(', ')}`);
 
       if (DEBUG) setSidebar(debug);
 
@@ -163,86 +161,99 @@ function gameTick() {
 
     if (currentTick % SIDEBAR_TIMEOUT === 0) cycleSidebar();
 
-    if (DEBUG) world.sendMessage(`${COLOR}[gameTick#0]`);
+    // log(`${COLOR}[gameTick#0]`);
 
-    incrementTicks();
+    // incrementTicks();
 
-    if (DEBUG) world.sendMessage(`${COLOR}[gameTick#1]`);
+    // log(`${COLOR}[gameTick#1]`);
 
-    checkLevelUp();
+    // checkLevelUp();
 
-    if (DEBUG) world.sendMessage(`${COLOR}[gameTick#2]`);
+    // log(`${COLOR}[gameTick#2]`);
   } catch (error) {
-    world.sendMessage(`${COLOR}[gameTick] ${COLOR_ERROR}Error: §r` + error);
+    log(`${COLOR}[gameTick] ${COLOR_ERROR}Error: §r` + error);
   }
 
-  system.run(gameTick);
+  server.system.run(gameTick);
 }
 
-function setSidebar(objective: ScoreboardObjective) {
+function setSidebar(objective: server.ScoreboardObjective) {
   try {
-    if (DEBUG) world.sendMessage(`${COLOR}[setSidebar] Setting sidebar to §r${objective.displayName}`);
+    log(`${COLOR}[setSidebar] Setting sidebar to §r${objective.displayName}`);
 
-    world.scoreboard.setObjectiveAtDisplaySlot(DisplaySlotId.Sidebar, {
+    server.world.scoreboard.setObjectiveAtDisplaySlot(server.DisplaySlotId.Sidebar, {
       objective: objective,
     });
   } catch (error) {
-    world.sendMessage(`${COLOR}[setSidebar] ${COLOR_ERROR}Error: §r` + error);
+    log(`${COLOR}[setSidebar] ${COLOR_ERROR}Error: §r` + error);
   }
 }
 
 function incrementTicks() {
   try {
-    if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#0]`);
+    log(`${COLOR}[incrementTicks#0]`);
 
-    const players = world.getAllPlayers();
+    const players = server.world.getAllPlayers();
 
-    if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#1]`);
+    log(`${COLOR}[incrementTicks#1]`);
 
     for (const player of players) {
+      log(`${COLOR}[incrementTicks#1a]`);
+
       if (!player) {
-        if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks] ${COLOR_ERROR}Player not found`);
-        continue;
+        log(`${COLOR}[incrementTicks] ${COLOR_ERROR}Player not found`);
+        // continue;
       }
 
-      if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#2]`);
+      log(`${COLOR}[incrementTicks#1b]`);
+
+      const playerIdentity = player.scoreboardIdentity;
+
+      log(`${COLOR}[incrementTicks#1c]`);
+
+      if (!playerIdentity) {
+        log(`${COLOR}[incrementTicks] ${COLOR_ERROR}Player identity not found`);
+        // continue;
+      }
+
+      log(`${COLOR}[incrementTicks#2]`);
 
       const score = ticks.getScore(player) || 0;
 
-      if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#3]`);
+      log(`${COLOR}[incrementTicks#3]`);
 
       if (score % HOUR === 0) {
-        if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#4]`);
+        log(`${COLOR}[incrementTicks#4]`);
 
         const hours = score / HOUR;
 
-        if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#5]`);
+        log(`${COLOR}[incrementTicks#5]`);
 
         hours_played.setScore(player, hours);
 
-        if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#6]`);
+        log(`${COLOR}[incrementTicks#6]`);
 
-        if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks] ${player.name} has played for §r${hours} ${COLOR}hours!`);
+        log(`${COLOR}[incrementTicks] ${player.name} has played for §r${hours} ${COLOR}hours!`);
 
-        if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#7]`);
+        log(`${COLOR}[incrementTicks#7]`);
 
         ticks.setScore(player, 1);
 
-        if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#8]`);
+        log(`${COLOR}[incrementTicks#8]`);
       } else {
-        if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#9]`);
+        log(`${COLOR}[incrementTicks#9]`);
 
         ticks.setScore(player, score + 1);
 
-        if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#10]`);
+        log(`${COLOR}[incrementTicks#10]`);
       }
 
-      if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#11]`);
+      log(`${COLOR}[incrementTicks#11]`);
     }
 
-    if (DEBUG) world.sendMessage(`${COLOR}[incrementTicks#12]`);
+    log(`${COLOR}[incrementTicks#12]`);
   } catch (error) {
-    world.sendMessage(`${COLOR}[incrementTicks] ${COLOR_ERROR}Error: §r` + error);
+    log(`${COLOR}[incrementTicks] ${COLOR_ERROR}Error: §r` + error);
   }
 }
 
@@ -250,56 +261,67 @@ function cycleSidebar() {
   try {
     const currentSidebarIndex = ((debug.getScore('sidebard_index') || 0) + 1) % sidebarObjectiveArray.length;
 
-    if (DEBUG) world.sendMessage(`${COLOR}[cycleSidebar] currentSidebarIndex: §r${currentSidebarIndex}`);
+    log(`${COLOR}[cycleSidebar] currentSidebarIndex: §r${currentSidebarIndex}`);
 
     setSidebar(sidebarObjectiveArray[currentSidebarIndex]);
     debug.setScore('sidebard_index', currentSidebarIndex);
   } catch (error) {
-    world.sendMessage(`${COLOR}[cycleSidebar] ${COLOR_ERROR}Error: §r` + error);
+    log(`${COLOR}[cycleSidebar] ${COLOR_ERROR}Error: §r` + error);
   }
 }
 
 function checkLevelUp() {
   try {
-    if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp#0]`);
+    log(`${COLOR}[checkLevelUp#0]`);
 
-    const players = world.getAllPlayers();
+    const players = server.world.getAllPlayers();
 
-    if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp#1]`);
+    log(`${COLOR}[checkLevelUp#1]`);
 
     for (const player of players) {
       if (!player) {
-        if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp] ${COLOR_ERROR}Player not found`);
-        continue;
+        log(`${COLOR}[checkLevelUp] ${COLOR_ERROR}Player not found`);
+        // continue;
       }
 
-      if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp#2]`);
+      log(`${COLOR}[checkLevelUp#1a]`);
+
+      const playerIdentity = player.scoreboardIdentity;
+
+      log(`${COLOR}[checkLevelUp#1b]`);
+
+      if (!playerIdentity) {
+        log(`${COLOR}[checkLevelUp] ${COLOR_ERROR}Player identity not found`);
+        // continue;
+      }
+
+      log(`${COLOR}[checkLevelUp#2]`);
 
       const currentLevel = player.level;
-      if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp#3]`);
+      log(`${COLOR}[checkLevelUp#3]`);
 
       const maxLevel = max_level.getScore(player) || 0;
-      if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp#4]`);
+      log(`${COLOR}[checkLevelUp#4]`);
 
       if (currentLevel > maxLevel) {
-        if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp#5]`);
+        log(`${COLOR}[checkLevelUp#5]`);
 
         max_level.setScore(player, currentLevel);
 
-        if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp#6]`);
+        log(`${COLOR}[checkLevelUp#6]`);
 
-        if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp] ${player.name} has reached level §r${currentLevel}!`);
+        log(`${COLOR}[checkLevelUp] ${player.name} has reached level §r${currentLevel}!`);
 
-        if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp#7]`);
+        log(`${COLOR}[checkLevelUp#7]`);
       }
 
-      if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp#8]`);
+      log(`${COLOR}[checkLevelUp#8]`);
 
       current_level.setScore(player, currentLevel);
 
-      if (DEBUG) world.sendMessage(`${COLOR}[checkLevelUp#9]`);
+      log(`${COLOR}[checkLevelUp#9]`);
     }
   } catch (error) {
-    world.sendMessage(`${COLOR}[checkLevelUp] ${COLOR_ERROR}Error: §r` + error);
+    log(`${COLOR}[checkLevelUp] ${COLOR_ERROR}Error: §r` + error);
   }
 }
